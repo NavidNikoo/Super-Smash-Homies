c6f0df65822f19ad2695f9532dab3ea55a97b444
Navid Nikoo <121324067+NavidNikoo@users.noreply.github.com>
2024-10-03 09:08:23 -0700

    Add files via upload

---
+++ BirchEngine - Textures/BirchEngine/Src/Game.cpp
@@ -0,0 +1,137 @@
+#include "Game.h"
+#include "TextureManager.h"
+#include "Map.h"
+#include "ECS/Components.h"
+#include "Vector2D.h"
+#include "Collision.h"
+
+Map* map;
+Manager manager;
+
+SDL_Renderer* Game::renderer = nullptr;
+SDL_Event Game::event;
+
+std::vector<ColliderComponent*> Game::colliders;
+
+auto& player(manager.addEntity());
+auto& wall(manager.addEntity());
+
+enum groupLabels : std::size_t
+{
+	groupMap,
+	groupPlayers,
+	groupEnemies,
+	groupColliders
+};
+
+
+Game::Game()
+{}
+
+Game::~Game()
+{}
+
+void Game::init(const char* title, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+	
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+
+		isRunning = true;
+	}
+
+	map = new Map();
+
+	Map::LoadMap("assets/map16x16.png", 16, 16);
+
+	player.addComponent<TransformComponent>(2);
+	player.addComponent<SpriteComponent>("assets/sasuke_Idle.png", 2, 200);
+	player.addComponent<KeyboardController>();
+	player.addComponent<ColliderComponent>("player");
+	player.addGroup(groupPlayers);
+	wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
+	wall.addComponent<SpriteComponent>("assets/dirt.png");
+	wall.addComponent<ColliderComponent>("wall");
+	wall.addGroup(groupMap);
+}
+
+void Game::handleEvents()
+{
+
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT :
+		isRunning = false;
+	 	break;
+	default:
+		break;
+	}
+
+}
+
+void Game::update()
+{
+	manager.refresh();
+	manager.update();
+
+	for (auto cc : colliders)
+	{
+		Collision::AABB(player.getComponent<ColliderComponent>(), *cc);
+		
+	}
+}
+	
+auto& tiles(manager.getGroup(groupMap));
+auto& players(manager.getGroup(groupPlayers));
+auto& enemies(manager.getGroup(groupEnemies));
+
+void Game::render()
+{
+	//painters alg: whatever you paint first is background, then you go over the top of it
+	//tile mats and backgrounds, then players, enemies, and items last, over the background
+
+	SDL_RenderClear(renderer);
+	for (auto& t : tiles)
+	{
+		t->draw();
+	}
+	for (auto& p : players)
+	{
+		p->draw();
+	}
+	for (auto& e : enemies)
+	{
+		e->draw();
+	}
+
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
+
+
+void Game::AddTile(int id, int x, int y)
+{
+	auto& tile(manager.addEntity());
+	tile.addComponent<TileComponent>(x, y, 32, 32, id);
+	tile.addGroup(groupMap);
+}
